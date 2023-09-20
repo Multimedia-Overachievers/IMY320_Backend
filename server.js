ef3498b93154d8eef5bdc93768b416f24014b6bf
Francois <contact@francois-smith.com>
2023-09-20 15:47:15 +0200

    Backen Expansion

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 var app = express();
+
 var fs = require("fs")
+var bodyParser = require('body-parser')
+
+var cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 var server = app.listen(5000, function () {
     var host = server.address().address
@@ -10,14 +18,117 @@ var server = app.listen(5000, function () {
 
 app.get('/modules', function (req, res) {
     fs.readFile( "json/modules.json", 'utf8', function (err, data) {
-       console.log("Modules Requested");
        res.end(data);
     });
 });
 
 app.get('/questions', function (req, res) {
     fs.readFile( "json/questions.json", 'utf8', function (err, data) {
-       console.log("Questions Requested");
        res.end(data);
     });
 });
+
+app.get('/set-all-finished', function (req, res) {
+    fs.readFile( "json/questions.json", 'utf8', function (err, data) {
+        var data = JSON.parse(data);
+        data.module.forEach(module => {
+            module.chapters.forEach(chapter => {
+                chapter.questions.forEach(question => {
+                    question.finished = true;
+                });
+            });
+        });
+
+        fs.writeFile("json/questions.json", JSON.stringify(data, null, 4), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        
+            console.log("The file was saved! | Set all finished");
+        });
+    });
+});
+
+app.get('/set-all-unfinished', async function (req, res) {
+    writeQueue.push(req);
+    fs.readFile( "json/questions.json", 'utf8', function (err, data) {
+        var data = JSON.parse(data);
+        data.module.forEach(module => {
+            module.chapters.forEach(chapter => {
+                chapter.questions.forEach(question => {
+                    question.finished = false;
+                });
+            });
+        });
+
+        fs.writeFile("json/questions.json", JSON.stringify(data, null, 4), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        
+            console.log("The file was saved! | Set all unfinished");
+        });
+    });
+});
+
+app.post('/update-chapter-question', function (req, res) {
+    fs.readFile( "json/questions.json", 'utf8', function (err, data) {
+        var data = JSON.parse(data);
+        req.body.questionIndex.forEach(questionIndex => {
+            data.module[req.body.moduleIndex].chapters[req.body.chapterIndex].questions[questionIndex].finished = true;
+        });
+
+        fs.writeFile("json/questions.json", JSON.stringify(data, null, 4), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+
+        console.log("Question Set to finished");
+    });
+
+    res.json({});
+});
+
+
+app.post('/add-quiz-score', function (req, res) {
+    fs.readFile( "json/modules.json", 'utf8', function (err, data) {
+        var data = JSON.parse(data);
+        var module = data.data[req.body.moduleIndex];
+        var chapter = module.chapters[req.body.chapterIndex];
+
+        chapter.scores.push(parseInt(req.body.score));
+        module.timeSpent += parseInt(req.body.quizTime);
+        console.log("time spent", module.timeSpent);
+        // fs.writeFile("json/modules.json", JSON.stringify(data, null, 4), function(err) {
+        //     if(err) {
+        //         return console.log(err);
+        //     }
+        // });
+
+        console.log("Score added");
+    });
+
+    res.json({});
+});
+
+app.get('/clear-scores', function (req, res) {
+    fs.readFile( "json/modules.json", 'utf8', function (err, data) {
+        //for each chapter, set scores to empty array
+        var data = JSON.parse(data);
+        data.data.forEach(module => {
+            module.chapters.forEach(chapter => {
+                chapter.scores = [];
+            });
+        });
+
+        fs.writeFile("json/modules.json", JSON.stringify(data, null, 4), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+
+        console.log("Scores cleared");
+    });
+});
+
